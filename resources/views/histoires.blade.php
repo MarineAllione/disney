cee1c8318d2e28bb54fcab436e32838fc8ac3e4c
Marine <marineallione4243@gmail.com>
2019-04-10 19:27:04 +0200

    ras

--- resources/views/histoires.blade.php
+++ resources/views/histoires.blade.php
@@ -26,8 +26,8 @@
 
     <div class="component">
   <ul class="align">
-
-      @foreach($histoires as $histoire)                    <!-- Book 1 -->
+                            <!-- Book 1 -->
+      @foreach($histoires as $histoire)
     <li>
       <figure class='book'>
         <!-- Front -->
@@ -70,13 +70,10 @@
 <form>
     <div class="content">
       <div class="title m-b-md">
-        <img type = "button" src="img/retour.png"  onclick = "history.back()">
+        <img type = "button" src="img/retour.png" onclick = "history.back()">
       </div>
     </div>
 </form>
-
-
-
                       <!--script-->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>

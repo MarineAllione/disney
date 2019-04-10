54497771818f575c518f5f569c8d4c49ddc2aebb
Marine <marineallione4243@gmail.com>
2019-04-10 14:12:17 +0200

    mis code et btn retour fonctionnel

--- resources/views/histoires.blade.php
+++ resources/views/histoires.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Histoires</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link type="text/css" href="css/book.css" rel="stylesheet">
 
     <!-- Styles -->
@@ -25,7 +26,7 @@
 
     <div class="component">
   <ul class="align">
-    <!-- Book 1 -->
+                          <!-- Book 1 -->
     <li>
       <figure class='book'>
         <!-- Front -->
@@ -61,5 +62,20 @@
   </ul>
 </div>
 
+                      <!-- btn retour-->
+<form>
+    <div class="content">
+      <div class="title m-b-md">
+        <img type = "button" src="img/retour.png"  onclick = "history.back()">
+      </div>
+    </div>
+</form>
+
+
+
+                      <!--script-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>

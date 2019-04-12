f828b267b85fa0dc2967385ba9967b979196f740
Marine <marineallione4243@gmail.com>
2019-04-12 14:00:44 +0200

    app change ment ligne 46

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Mon compte') }}</a>
                             </li>
-                            @if (Route::has('register'))
+                            @if (Route::has('Créer mon compte'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Créer mon compte') }}</a>
                                 </li>
@@ -58,7 +58,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Deconnection') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">

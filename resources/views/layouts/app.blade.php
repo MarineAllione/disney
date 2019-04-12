5a0560825381bcfde6d809e43befc9dd0efbfb59
Marine <marineallione4243@gmail.com>
2019-04-12 14:59:52 +0200

    enlever le mot laravel remplacer par disney

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Disney <!--{{ config('app.name') }}--></title>
+    <title>Disney</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>

1c27accea416284b57fc431daea75e6f07f7229b
Marine <marineallione4243@gmail.com>
2019-04-12 14:12:41 +0200

    remplacer tous les authentification en français

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,12 +69,12 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Mon espace</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Mon compte</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Créer mon compte</a>
                         @endif
                     @endauth
                 </div>
